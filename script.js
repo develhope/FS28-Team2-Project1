9f2e3187a708af422f6273d839307c2b1ba70fa2
mario-sica/Mario <mario97sica@gmail.com>
2024-04-26 17:18:33 +0200

    Added: script.js with first object

---
+++ script.js
@@ -0,0 +1,40 @@
+// Crea un array di oggetti con i seguenti parametri:
+// - name
+// - surname
+// - age
+// - city
+// - hobby
+// - favoriteFood
+// - favoriteVideoGame
+// - favoriteFilm
+// - favoriteBook
+// - petName
+
+const people = [
+  {
+    name: 'Mario',
+    surname: 'Sica',
+    age: 26,
+    city: 'San Ferdinando di Puglia',
+    hobby: [
+      {
+        id: 1,
+        name: 'Gym'
+      },
+      {
+        id: 2,
+        name: 'Cars'
+      },
+      {
+        id: 3,
+        name: 'IT'
+      }
+    ],
+    favoriteFood: 'Parmigiana',
+    favoriteVideoGame: 'Age of Empires - Saga',
+    favoriteFilm: 'Inception',
+    favoriteBook: 'Odissea',
+    petName: 'Lilo'
+  }
+  // Aggiungi nuovi oggetti qui!
+];
