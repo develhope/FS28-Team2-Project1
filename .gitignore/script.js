6d21e0ff52e205d5fe9c8e785f1a3658702f60fb
Luigi Cavalli <luigi_cavalli@outlook.com>
2024-05-02 17:58:58 +0200

    feat: added functions

--- .gitignore/script.js
+++ .gitignore/script.js
@@ -56,88 +56,39 @@ const people = [
       }
     ],
     favoriteFood: 'Pizza',
-    favoriteVideoGame: 'The Last Of Us',
+    favoriteVideoGame: 'The Last of Us',
     favoriteFilm: 'The Truman Show',
     favoriteBook: 'Harry Potter and The Chamber of Secrets',
     petName: 'Bua'
-  },
-  {
-    name: 'Marzia',
-    surname: 'Bragagnolo',
-    age: 29,
-    city: 'Resana',
-    hobby: [
-      {
-        id: 1,
-        name: 'Crocheting'
-      },
-      {
-        id: 2,
-        name: 'Games'
-      },
-      {
-        id: 3,
-        name: 'Wood art'
-      }
-    ],
-    favoriteFood: 'Gnocchi',
-    favoriteVideoGame: "Assassin's creed",
-    favoriteFilm: 'Una settimana da Dio',
-    favoriteBook: "La Passe-Miroir Livre 4, La tempête des écho",
-    petName: 'Uga'
-  },
-  {
-    name: 'Lorenzo',
-    surname: 'de Vita',
-    age: 23,
-    city: 'Napoli',
-    hobby: [
-      {
-        id: 1,
-        name: 'Videogames'
-      },
-      {
-        id: 2,
-        name: 'Music'
-      },
-      {
-        id: 3,
-        name: 'Cinema'
-      }
-    ],
-    favoriteFood: 'Pizza',
-    favoriteVideoGame: 'Baldur\'s gate 3',
-    favoriteFilm: 'Lord of the rings',
-    favoriteBook: 'Zanna bianca',
-    petName: ''
-  },
+  }
   // Aggiungi nuovi oggetti qui!
 ];
 
-
-//Stampa i nomi in ordine alfabetico (surname name).
+// Stampa i nomi in ordine alfabetico (surname name).
 const sortedByName = people.slice().sort((a, b) => {
   if (a.surname === b.surname) {
     return a.name.localeCompare(b.name);
   }
   return a.surname.localeCompare(b.surname);
 });
-console.log('Team in alphabetical order:');
-sortedByName.forEach((person) =>
-  console.log(`${person.surname} ${person.name}`)
-);
+console.log('Team names in alphabetical order:');
+sortedByName.forEach((person) => {
+  console.log(`${person.surname} ${person.name}`);
+});
 
 // Stampa i nomi in ordine di età (name age).
 const sortedByAge = people.slice().sort((a, b) => a.age - b.age);
-console.log('\nTeam in age order:');
-sortedByAge.forEach((person) => console.log(`${person.name} ${person.age}`));
+console.log('Team names by age:');
+sortedByAge.forEach((person) => {
+  console.log(`${person.name} ${person.age}`);
+});
 
 // Stampa l'età media.
 const middleAge = people.reduce((acc, person) => acc + person.age, 0) / people.length;
-console.log(`\nTeam's middle age: ${middleAge}`);
+console.log(`Team's middle age: ${middleAge}`);
 
 //Stampa chi ha un animale domestico (name petName).
-console.log('\nMembers who have a pet:');
+console.log('Members who have a pet:');
 const peopleWithPets = people.slice().filter((person) => person.petName);
 if (peopleWithPets.length > 0) {
   peopleWithPets.forEach((person) => {
@@ -146,3 +97,23 @@ if (peopleWithPets.length > 0) {
 } else {
   console.log('Nobody in the team has a pet.');
 }
+
+// Stampa chi ha scritto 'LOL' o 'League of Legends come videogioco preferito (name).
+const peopleWhoLikeLol = people.slice().filter((person) => {
+  return (
+    person.favoriteVideoGame.toLowerCase() === 'lol' ||
+    person.favoriteVideoGame.toLowerCase() === 'league of legends'
+  );
+});
+console.log('Members who put League of Legends as favorite videogame:');
+if (peopleWhoLikeLol > 0) {
+  peopleWhoLikeLol.forEach((person) => {
+    console.log(`${person.name} likes the videogame "League of Legends".`);
+  });
+} else {
+  console.log(
+    'Nobody in the team put League of Legends as favorite videogame.'
+  );
+}
+
+// Stampa se ci sono membri con lo stesso nome (name).
