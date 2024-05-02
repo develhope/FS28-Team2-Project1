0a2461b18c52f9f986c73f0220ad2dcf9c734bbf
DEVita42 <dvt.lorenzo@gmail.com>
2024-05-02 15:30:28 +0200

    indentation corrected

--- team.js
+++ team.js
@@ -1,12 +1,14 @@
-const person = [{
-    name: "Lorenzo",
-    surname: "de Vita",
+const person = [
+  {
+    name: 'Lorenzo',
+    surname: 'de Vita',
     age: 23,
-    city: "Napoli",
-    hobby: "Videogiochi",
-    favoriteFood: "Pizza",
+    city: 'Napoli',
+    hobby: 'Videogiochi',
+    favoriteFood: 'Pizza',
     favoriteVideogame: "Baldur's gate 3",
-    favoriteFilm: "Il signore degli anelli",
-    favoriteBook: "Zanna bianca",
-    petName: ""
-}]
+    favoriteFilm: 'Il signore degli anelli',
+    favoriteBook: 'Zanna bianca',
+    petName: ''
+  }
+];
