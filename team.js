7e7ee256deb1926c7512f6b30d1479fce98f4ea8
DEVita42 <dvt.lorenzo@gmail.com>
2024-05-02 15:09:28 +0200

    Added: object Lorenzo in people

---
+++ team.js
@@ -0,0 +1,12 @@
+const person = [{
+    name: "Lorenzo",
+    surname: "de Vita",
+    age: 23,
+    city: "Napoli",
+    hobby: "Videogiochi",
+    favoriteFood: "Pizza",
+    favoriteVideogame: "Baldur's gate 3",
+    favoriteFilm: "Il signore degli anelli",
+    favoriteBook: "Zanna bianca",
+    petName: ""
+}]
